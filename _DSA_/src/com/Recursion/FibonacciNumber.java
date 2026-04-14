3ae01fc4feee8b90cfe25715b3a2274fee512b68
Chaitanya-963 <chaitanyamehetre0301@gmail.com>
2026-04-14 14:52:37 +0530

    Learning Recursion

--- _DSA_/src/com/Recursion/FibonacciNumber.java
+++ _DSA_/src/com/Recursion/FibonacciNumber.java
@@ -1,4 +1,18 @@
 package com.Recursion;
 
+//fibo(N) = fibo(N-1) + fibo(N-2);
+
 public class FibonacciNumber {
+    public static void main(String[] args) {
+        System.out.println(fibo(6));
+
+    }
+
+    static int fibo(int N) {
+        if (N == 0 || N == 1) {
+            return N;
+        }
+
+        return fibo(N - 1) + fibo(N - 2);
+    }
 }
--- _DSA_/src/com/Recursion/NumberExampleRecursion.java
+++ _DSA_/src/com/Recursion/NumberExampleRecursion.java
@@ -1,4 +1,20 @@
 package com.Recursion;
 
 public class NumberExampleRecursion {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+    static void print(int n) {
+        // Base condition
+        if (n == 5) {
+            System.out.println(n);
+            return;
+        }
+
+        // recursive call
+        // if you are calling a function again and again, you can treat it as a separate call in the stack
+        System.out.print(n + " ");
+        print(n + 1);
+    }
 }
