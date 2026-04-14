e8fae7ea0f901af84dce69f7b5edfdc6faca50aa
Chaitanya-963 <chaitanyamehetre0301@gmail.com>
2026-04-14 14:50:25 +0530

    Learn Cyclic sort and solve problems

--- _DSA_/src/com/CyclicSort/CyclicSort.java
+++ _DSA_/src/com/CyclicSort/CyclicSort.java
@@ -1,4 +1,34 @@
 package com.CyclicSort;
 
+/*  ⭐⭐⭐⭐⭐ Pattern
+    Asked in amazon interview questions also
+ -- When given numbers from range 1,N ==> Use Cyclic sort
+ */
+
+import java.util.Arrays;
+
 public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 4, 3, 6, 7, 1};
+        cyclic(arr);
+        System.out.println("Cyclic Sort Method: " + Arrays.toString(arr));
+    }
+
+    public static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static void cyclic(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+    }
 }
