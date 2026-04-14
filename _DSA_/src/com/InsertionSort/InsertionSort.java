02aaf5030794f5a945878f309b786053afe56b9e
Chaitanya-963 <chaitanyamehetre0301@gmail.com>
2026-04-14 14:50:52 +0530

    Learn Insertion sort and solve problems

--- _DSA_/src/com/InsertionSort/InsertionSort.java
+++ _DSA_/src/com/InsertionSort/InsertionSort.java
@@ -1,4 +1,46 @@
 package com.InsertionSort;
 
+import java.util.Arrays;
+
 public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {50, 22, 4, 23, 6, 7, 1};
+        insertionSort(arr);
+        System.out.println("Insertion Sort Method: " + Arrays.toString(arr));
+
+    }
+
+    static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+
+        }
+    }
 }
